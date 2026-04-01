b3f6c4c7aafe3d10bd2a8e606409b2421b41fa3f
Giglo01 <negigitesh@gmail.com>
2026-04-01 21:12:46 +0530

    asdf

---
+++ src/Controller/usercontrolls.js
@@ -0,0 +1,8 @@
+ exports.evenOdd = (num)=>{
+    if (num%2==0){
+        return'even'
+    }
+    else{
+        return 'odd'
+    }
+ }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose")
-const express = require("express")
+const mongoose = require ('mongoose')
+const express = require ('express')
+const {evenOdd} = require('./Controller/usercontrolls')
 
 const app = express()
 const port = 5000;
 
-const mongourl = ""
+const mongourl = 'mongodb+srv://negigitesh_db_user:GlV0rvqZnOIvw5ql@gitesh.1ziv0xy.mongodb.net/'
 mongoose.connect(mongourl)
 
-.then(()=>{console.log(``)})
-.catch((e)=>{console.log(`${e}`)})
-app.listen(port,()=>{console.log(`${port}`);})
+.then(()=>{console.log('The mongoDB is connected suceeesfully')})
+.catch((e)=>{console.log(`There is an error: ${e}`)})
+app.listen(port,()=>{console.log(`The server is running the port: ${port}`)})
+
+console.log(evenOdd(2))
